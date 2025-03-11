0efd85eb82fb9118b6c8d5a8561066254c52e0b4
qg7771481 <ddavidok845@gmail.com>
2025-03-11 20:43:22 +0100

    models.py

--- models.py
+++ models.py
@@ -1,14 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from flask_login import UserMixin
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///python_quiz.db'
 db = SQLAlchemy(app)
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
 
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
