010444604b0c63b2bec0daa9bca0f35104866dd6
qg7771481 <ddavidok845@gmail.com>
2025-03-21 14:13:17 +0100

    models.py

--- models.py
+++ models.py
@@ -6,16 +6,25 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///python_quiz.db'
 db = SQLAlchemy(app)
 
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(256), nullable=False)
+    is_verified = db.Column(db.Boolean, default=False)
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
 
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.Text, nullable=False)
     correct_answer = db.Column(db.String(255), nullable=False)
+
 
 class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,6 +32,7 @@ class Answer(db.Model):
     text = db.Column(db.String(255), nullable=False)
     is_correct = db.Column(db.Boolean, default=False)
     question = db.relationship('Question', backref=db.backref('answers', lazy=True))
+
 
 class Result(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -32,6 +42,7 @@ class Result(db.Model):
     is_correct = db.Column(db.Boolean, nullable=False)
     user = db.relationship('User', backref=db.backref('results', lazy=True))
     question = db.relationship('Question', backref=db.backref('results', lazy=True))
+
 
 if __name__ == "__main__":
     with app.app_context():
